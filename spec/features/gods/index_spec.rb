6889cbf6c0e524a081afbb40dce3c64caba36507
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-24 21:17:45 -0400

    test gods#index link to gods#new

--- spec/features/gods/index_spec.rb
+++ spec/features/gods/index_spec.rb
@@ -39,4 +39,19 @@ RSpec.describe 'God Index Page' do
     expect(page).to have_link("Monsters", :href => "/monsters/")
   end
 
+  # As a visitor
+  # When I visit the Parent Index page
+  # Then I see a link to create a new Parent record, "New Parent"
+  # When I click this link
+  # Then I am taken to '/parents/new' where I  see a form for a new parent record
+  # When I fill out the form with a new parent's attributes:
+  # And I click the button "Create Parent" to submit the form
+  # Then a `POST` request is sent to the '/parents' route,
+  # a new parent record is created,
+  # and I am redirected to the Parent Index page where I see the new Parent displayed.
+  it 'links to a page allowing the creation of new parents' do
+    visit "/gods"
+
+    expect(page).to have_link("Recognize a new God", :href => "/gods/new/")
+  end  
 end
