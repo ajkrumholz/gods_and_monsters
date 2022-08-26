56e8a6e7a199a3570cbff5cc23050c6feea6a976
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-26 08:50:43 -0400

    dry out monsters#edit testing using before each

--- spec/features/gods/edit_spec.rb
+++ spec/features/gods/edit_spec.rb
@@ -1,42 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Monster update' do
-  it 'links to home, gods, and monsters' do
-    khorne = God.create(name: "Khorne", age: 20, immortal: false)
-
+  before(:each) do
+    @khorne = God.create(name: "Khorne", age: 20, immortal: false)
     visit "/gods/#{khorne.id}/edit"
-
+  end
+  
+  it 'links to home, gods, and monsters' do
     expect(page).to have_link("Home", :href => "/")
     expect(page).to have_link("Gods", :href => "/gods/")
     expect(page).to have_link("Monsters", :href => "/monsters/")
   end
 
   it 'has a form to define the attributes of the new god' do
-    khorne = God.create(name: "Khorne", age: 20, immortal: false)
-    
-    visit "/gods/#{khorne.id}/edit"
-
     expect(page).to have_field(:name)
     expect(page).to have_field(:age)
     expect(page).to have_field(:immortal_true)
     expect(page).to have_field(:immortal_false)
-    expect(page).to have_button("Update #{khorne.name}")
+    expect(page).to have_button("Update #{@khorne.name}")
   end
 
   it 'updates a god using the form' do
-    khorne = God.create(name: "Khorne", age: 20, immortal: false)
-    
-    visit "/gods/#{khorne.id}/edit"
-
     fill_in(:name, with: 'Khorne')
     fill_in(:age, with: '21')
     choose(:immortal_false)
-    click_on("Update #{khorne.name}")
+    click_on("Update #{@khorne.name}")
     @god = God.order(:updated_at).last
 
     expect(@god.name).to eq('Khorne')
     expect(@god.age).to eq(21)
     expect(@god.immortal).to eq(false)
-    expect(page).to have_current_path("/gods/#{khorne.id}")
+    expect(page).to have_current_path("/gods/#{@khorne.id}")
   end
 end
