68c746446c42f9f3904d692eed44f9522a8b40a2
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-26 08:31:23 -0400

    add monster creation feature in gods#menagerie

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -4,10 +4,27 @@ class MonstersController < ApplicationController
   end
 
   def new
-
+    @god = God.find(params[:id])
   end
 
   def show
     @monster = Monster.find(params[:id])
+  end
+
+  def create
+    @god = God.find(params[:god_id])
+    @monster = @god.monsters.new(
+      {
+        name: params[:name],
+        strength_rank: params[:strength_rank],
+        flying: params[:flying]
+      }
+    )
+    if @monster.save
+      redirect_to "/gods/#{@god.id}/menagerie"
+    else
+      redirect_to "/gods/#{@god.id}/menagerie/new"
+      @errors = @monster.errors.messages
+    end
   end
 end
--- spec/features/gods/menagerie_spec.rb
+++ spec/features/gods/menagerie_spec.rb
@@ -1,34 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'The Menagerie of a God' do
-  it 'shows all the monsters and their attributes associated with a specific god' do
-    khorne = God.create(name: "Khorne", age: 20, immortal: false)
-    bloodthirster = khorne.monsters.create!(name: "Bloodthirster", strength_rank: 8.5, flying: true)
-    hellhound = khorne.monsters.create!(name: "Hellhound", strength_rank: 5.5, flying: false)
-
-    visit "/gods/#{khorne.id}/menagerie"
-
-    expect(page).to have_content(bloodthirster.name)
-    expect(page).to have_content(bloodthirster.strength_rank)
-    expect(page).to have_content(bloodthirster.flying)
-    expect(page).to have_content(bloodthirster.god.name)
-    expect(page).to have_content(bloodthirster.created_at)
-    expect(page).to have_content(bloodthirster.updated_at)
-    expect(page).to have_content(hellhound.name)
-    expect(page).to have_content(hellhound.strength_rank)
-    expect(page).to have_content(hellhound.flying)
-    expect(page).to have_content(hellhound.god.name)
-    expect(page).to have_content(hellhound.created_at)
-    expect(page).to have_content(hellhound.updated_at)
+ 
+  before(:each) do
+    @khorne = God.create(name: "Khorne", age: 20, immortal: false)
+    @bloodthirster = @khorne.monsters.create!(name: "Bloodthirster", strength_rank: 8.5, flying: true)
+    @hellhound = @khorne.monsters.create!(name: "Hellhound", strength_rank: 5.5, flying: false)
   end
 
-  it 'links to home, gods, and monsters' do
-    khorne = God.create(name: "Khorne", age: 20, immortal: false)
+  describe "gods#menagerie" do 
 
-    visit "/gods/#{khorne.id}/menagerie"
+    it 'shows all the monsters and their attributes associated with a specific god' do
 
-    expect(page).to have_link("Home", :href => "/")
-    expect(page).to have_link("Gods", :href => "/gods/")
-    expect(page).to have_link("Monsters", :href => "/monsters/")
+      visit "/gods/#{@khorne.id}/menagerie"
+
+      expect(page).to have_content(@bloodthirster.name)
+      expect(page).to have_content(@bloodthirster.strength_rank)
+      expect(page).to have_content(@bloodthirster.flying)
+      expect(page).to have_content(@bloodthirster.god.name)
+      expect(page).to have_content(@bloodthirster.created_at)
+      expect(page).to have_content(@bloodthirster.updated_at)
+      expect(page).to have_content(@hellhound.name)
+      expect(page).to have_content(@hellhound.strength_rank)
+      expect(page).to have_content(@hellhound.flying)
+      expect(page).to have_content(@hellhound.god.name)
+      expect(page).to have_content(@hellhound.created_at)
+      expect(page).to have_content(@hellhound.updated_at)
+    end
+
+    it 'links to home, gods, and monsters' do
+      visit "/gods/#{@khorne.id}/menagerie"
+
+      expect(page).to have_link("Home", :href => "/")
+      expect(page).to have_link("Gods", :href => "/gods/")
+      expect(page).to have_link("Monsters", :href => "/monsters/")
+    end
+
+    it 'has a link to create a new monster' do
+      visit "/gods/#{@khorne.id}/menagerie"
+
+      expect(page).to have_link("Summon New Monster", :href => "/gods/#{@khorne.id}/menagerie/new")
+    end
   end
 end
