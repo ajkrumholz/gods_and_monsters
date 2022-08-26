f4b73f46b1983e7a6555556e025943f595e8a1ae
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-26 14:56:33 -0400

    refactor menagerie_spec with #within

--- spec/features/gods/menagerie_spec.rb
+++ spec/features/gods/menagerie_spec.rb
@@ -6,6 +6,9 @@ RSpec.describe 'The Menagerie of a God' do
     @khorne = God.create(name: "Khorne", age: 20, immortal: false)
     @bloodthirster = @khorne.monsters.create!(name: "Bloodthirster", strength_rank: 8.5, flying: true)
     @hellhound = @khorne.monsters.create!(name: "Hellhound", strength_rank: 5.5, flying: false)
+    @skullcrushers = @khorne.monsters.create!(name: "Skullcrusher", strength_rank: 7.5, flying: false)
+    @monsters = [@bloodthirster, @hellhound]
+    
   end
 
   describe "gods#menagerie" do 
@@ -14,18 +17,15 @@ RSpec.describe 'The Menagerie of a God' do
 
       visit "/gods/#{@khorne.id}/menagerie"
 
-      expect(page).to have_content(@bloodthirster.name)
-      expect(page).to have_content(@bloodthirster.strength_rank)
-      expect(page).to have_content(@bloodthirster.flying)
-      expect(page).to have_content(@bloodthirster.god.name)
-      expect(page).to have_content(@bloodthirster.created_at)
-      expect(page).to have_content(@bloodthirster.updated_at)
-      expect(page).to have_content(@hellhound.name)
-      expect(page).to have_content(@hellhound.strength_rank)
-      expect(page).to have_content(@hellhound.flying)
-      expect(page).to have_content(@hellhound.god.name)
-      expect(page).to have_content(@hellhound.created_at)
-      expect(page).to have_content(@hellhound.updated_at)
+      @monsters.each do |monster|
+        within("#monster_#{monster.id}") do
+          expect(page).to have_content(monster.name)
+          expect(page).to have_content(monster.strength_rank)
+          expect(page).to have_content(monster.flying)
+          expect(page).to have_content(monster.created_at)
+          expect(page).to have_content(monster.updated_at)
+        end
+      end
     end
 
     it 'links to home, gods, and monsters' do
