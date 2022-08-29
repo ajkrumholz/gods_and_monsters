2b90f72ada01f54e81a98ab600549a7121cbd527
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-29 19:38:53 -0400

    create #display_by_str in monster.rb model, move from menagerie, add model testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,10 @@
 @ice_leopard = @ursun.monsters.create!(name: "Ice Leopard", strength_rank: 4.0, flying: false)
 @little_grom = @ursun.monsters.create!(name: "Little Grom", strength_rank: 6.5, flying: false)
 
+@daemonette = @slaanesh.monsters.create!(name: "Daemonette", strength_rank: 3.5, flying: false)
+@daemon_priestess = @slaanesh.monsters.create!(name: "Daemon Priestess", strength_rank: 9.0, flying: true)
+@soul_grinder = @slaanesh.monsters.create!(name: "Soul Grinder", strength_rank: 8.5, flying: false)
+
 @plague_drones = @nurgle.monsters.create!(name: "Plague Drones", strength_rank: 5.5, flying: true)
 @festus = @nurgle.monsters.create!(name: "Festus", strength_rank: 8.0, flying: false)
 @great_unclean = @nurgle.monsters.create!(name: "Great Unclean One", strength_rank: 7.5, flying: true)
@@ -29,7 +33,4 @@
 @altdorf_gryphites = @sigmar.monsters.create!(name: "Royal Altdorf Gryphites", strength_rank: 8.0, flying: true)
 @luminark = @sigmar.monsters.create!(name: "Luminark of Hysh", strength_rank: 7.5, flying: false)
 
-@daemonette = @slaanesh.monsters.create!(name: "Daemonette", strength_rank: 3.5, flying: false)
-@daemon_priestess = @slaanesh.monsters.create!(name: "Daemon Priestess", strength_rank: 9.0, flying: true)
-@soul_grinder = @slaanesh.monsters.create!(name: "Soul Grinder", strength_rank: 8.5, flying: false)
 
--- spec/models/monster_spec.rb
+++ spec/models/monster_spec.rb
@@ -1,26 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Monster do
-  let(:khorne) { God.create!(name: "Khorne", age: 20, immortal: true)}
-  let(:bloodthirster) {khorne.monsters.create!(name: "Bloodthirster", strength_rank: 9.5, flying: true)}
-
-  it 'exists' do
-    expect(bloodthirster).to be_a(Monster)
+  before(:each) do
+    @khorne = God.create(name: "Khorne", age: 40, immortal: true)
+    @bloodthirster = @khorne.monsters.create!(name: "Bloodthirster", strength_rank: 9.0, flying: true)
+    @hellhound = @khorne.monsters.create!(name: "Hellhound", strength_rank: 5.5, flying: false)
+    @chaos_furies = @khorne.monsters.create!(name: "Chaos Furies", strength_rank: 3.5, flying: true)
   end
-
-  it 'has a name' do
-    expect(bloodthirster.name).to eq("Bloodthirster")
-  end
-
-  it 'has a strength rank' do
-    expect(bloodthirster.strength_rank).to eq(9.5)
-  end
-
-  it 'has can be a flying monster' do
-    expect(bloodthirster.flying).to be(true)
-  end
-
-  it 'has a god' do
-    expect(bloodthirster.god).to be(khorne)
+  
+  it 'can display monsters above a given strength rank' do
+    expect(Monster.all.display_by_str(@khorne, 5)).to eq([@bloodthirster, @hellhound])
   end
 end
