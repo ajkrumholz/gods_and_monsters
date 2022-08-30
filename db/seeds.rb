b9ffcce8a7676d143cae91ac3745108c0d07f33c
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-30 17:55:13 -0400

    modify db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,18 @@
 @chaos_furies = @khorne.monsters.create!(name: "Chaos Furies", strength_rank: 3.5, flying: true)
 
 @elemental_bear = @ursun.monsters.create!(name: "Elemental Bear", strength_rank: 8.5, flying: false)
-@ice_leopard = @ursun.monsters.create!(name: "Ice Leopard", strength_rank: 4.0, flying: false)
-@little_grom = @ursun.monsters.create!(name: "Little Grom", strength_rank: 6.5, flying: false)
 
 @daemonette = @slaanesh.monsters.create!(name: "Daemonette", strength_rank: 3.5, flying: false)
 @daemon_priestess = @slaanesh.monsters.create!(name: "Daemon Priestess", strength_rank: 9.0, flying: true)
 @soul_grinder = @slaanesh.monsters.create!(name: "Soul Grinder", strength_rank: 8.5, flying: false)
+@harpies = @slaanesh.monsters.create!(name: "Harpies", strength_rank: 4.5, flying: true)
 
 @plague_drones = @nurgle.monsters.create!(name: "Plague Drones", strength_rank: 5.5, flying: true)
 @festus = @nurgle.monsters.create!(name: "Festus", strength_rank: 8.0, flying: false)
 @great_unclean = @nurgle.monsters.create!(name: "Great Unclean One", strength_rank: 7.5, flying: true)
 @nurglings = @nurgle.monsters.create!(name: "Nurglings", strength_rank: 2.5, flying: false)
+@marauders = @nurgle.monsters.create!(name: "Marauders", strength_rank: 4.0, flying: false)
 
-@deathclaw = @sigmar.monsters.create!(name: "Deathclaw", strength_rank: 9.0, flying: true)
 @altdorf_gryphites = @sigmar.monsters.create!(name: "Royal Altdorf Gryphites", strength_rank: 8.0, flying: true)
 @luminark = @sigmar.monsters.create!(name: "Luminark of Hysh", strength_rank: 7.5, flying: false)
 
